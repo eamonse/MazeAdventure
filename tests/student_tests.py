f9e62d15655cadcc0eba018f12a10653a48d0f30
EamonSenapati <90056436+EamonSenapati@users.noreply.github.com>
2023-01-02 23:54:52 -0800

    Update student_tests.py

--- tests/student_tests.py
+++ tests/student_tests.py
@@ -6,4 +6,25 @@ class maze_adventure_student_tests(TestCase):
         return super().setUp()
 
     def test_student_case(self):
-        pass
+        map = {
+            "A" : MazeRoom([{ROOM_KEY:"B", TIME_NEEDED_KEY:1}, {ROOM_KEY:"D", TIME_NEEDED_KEY: 1}]),
+            "B" : MazeRoom([{ROOM_KEY:"C", TIME_NEEDED_KEY:1}]),
+            "C" : MazeRoom([{ROOM_KEY:"A", TIME_NEEDED_KEY:1}, {ROOM_KEY:"D", TIME_NEEDED_KEY:2}]),
+            "D" : MazeRoom([])
+        }
+
+        maze = Maze(map, "A", "A")
+        answer = maze.solve_maze()
+        self.assertEqual(answer, None)
+
+        maze = Maze(map, "B", "D")
+        answer = maze.solve_maze()
+        round1 = answer.pop()
+        self.assertEqual(round1[ROOM_KEY], "D")
+        self.assertEqual(round1[TIME_TAKEN_TO_ARRIVE_HERE_KEY], 2)
+        round2 = answer.pop()
+        self.assertEqual(round2[ROOM_KEY], "C")
+        self.assertEqual(round2[TIME_TAKEN_TO_ARRIVE_HERE_KEY], 1)
+        round3 = answer.pop()
+        self.assertEqual(round3[ROOM_KEY], "B")
+        self.assertEqual(round3[TIME_TAKEN_TO_ARRIVE_HERE_KEY], 0)
